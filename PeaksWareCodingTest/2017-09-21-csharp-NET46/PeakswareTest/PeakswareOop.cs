f16029ff624d82ba478f5e4a22290860c1cc3636
neeraj <32710887+ENGNRD@users.noreply.github.com>
2017-10-21 15:24:27 -0600

    OOP implementation

--- PeaksWareCodingTest/2017-09-21-csharp-NET46/PeakswareTest/PeakswareOop.cs
+++ PeaksWareCodingTest/2017-09-21-csharp-NET46/PeakswareTest/PeakswareOop.cs
@@ -4,48 +4,217 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PeakswareTestOop
 {
-    class DataPoint
+    class DataPoint 
     {
+        public enum Type : int
+        {
+            DataRaw=0,
+            Data1MinuteAverage=1,
+            Data5MinuteAverage=2,
+            Data10MinuteAverage=3,
+            Data15MinuteAverage=4,
+            Data20MinuteAverage = 5
+        }
+
+        private ushort[] _dataValues = new ushort[6]; 
         public Double TimeOffset { get;  internal set; }
-        public ushort DataValue { get; internal set; }
-        public ushort DataValue1MinAvg { get; internal set; }
-        public ushort DataValue5MinAvg { get; internal set; }
-        public ushort DataValue10MinAvg { get; internal set; }
-        public ushort DataValue15MinAvg { get; internal set; }
-        public ushort DataValue20MinAvg { get; internal set; }
+
+        internal ushort[] DataValues
+        {
+            get
+            {
+                return _dataValues;
+            }
+        }
+        public ushort DataValueRaw 
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.DataRaw];
+            }
+            internal set
+            {
+                _dataValues[(int)DataPoint.Type.DataRaw] = value;
+            }
+        }
+
+        public ushort DataValue1MinAvg
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.Data1MinuteAverage];
+            }
+        }
+
+        public ushort DataValue5MinAvg
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.Data5MinuteAverage];
+            }
+        }
+
+        public ushort DataValue10MinAvg
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.Data10MinuteAverage];
+            }
+        }
+
+        public ushort DataValue15MinAvg
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.Data15MinuteAverage];
+            }
+        }
+
+        public ushort DataValue20MinAvg
+        {
+            get
+            {
+                return _dataValues[(int)DataPoint.Type.Data20MinuteAverage];
+            }
+        }
 
         public DataPoint(Double timeOffset, ushort dataValue)
         {
             TimeOffset = timeOffset;
-            DataValue = dataValue;
+            DataValueRaw = dataValue;
         }
+
+        public static bool operator ==(DataPoint dp1, DataPoint dp2)
+        {
+            if (dp1 != null && dp2 != null)
+                return dp1.TimeOffset == dp2.TimeOffset;
+            else
+                return false;
+        }
+
+        public static bool operator !=(DataPoint dp1, DataPoint dp2)
+        {
+            if (dp1 != null && dp2 != null)
+                return dp1.TimeOffset != dp2.TimeOffset;
+            else
+                return true;
+            
+        }
+
+        
     }
 
-    class ChannelData : Dictionary<double, DataPoint>  
+    class ChannelData : List<DataPoint>  
     {
+        private Object lockCollection = new Object();
+        
+        private int _threadCount;
         public string DataName { get; private set; }
+        public int ThreadCount 
+        {
+            get
+            {
+                return _threadCount;
+            }
+        }
 
         public ChannelData(string dataName)
         {
             DataName = dataName;
+            _threadCount = 0;
         }
 
         new public void Add(double timeOffset, DataPoint datapoint)
         {
-            base.Add(timeOffset, datapoint);
+            lock (lockCollection)
+            {
+                base.Add(datapoint);
+            }
+            CalculateAverages(datapoint);
             return;
+        }
+
+        private void CalculateAverages(DataPoint datapoint)
+        {
+
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60, DataPoint.Type.Data1MinuteAverage));
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60, DataPoint.Type.Data1MinuteAverage));
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60 * 5, DataPoint.Type.Data5MinuteAverage));
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60 * 10, DataPoint.Type.Data10MinuteAverage));
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60 * 15, DataPoint.Type.Data15MinuteAverage));
+            Task.Factory.StartNew(() => CalculateAveragesFor(datapoint, 60 * 20, DataPoint.Type.Data20MinuteAverage));
+
+            //CalculateAveragesFor(datapoint, 60, DataPoint.Type.Data1MinuteAverage);
+            //CalculateAveragesFor(datapoint, 60, DataPoint.Type.Data1MinuteAverage);
+            //CalculateAveragesFor(datapoint, 60 * 5, DataPoint.Type.Data5MinuteAverage);
+            //CalculateAveragesFor(datapoint, 60 * 10, DataPoint.Type.Data10MinuteAverage);
+            //CalculateAveragesFor(datapoint, 60 * 15, DataPoint.Type.Data15MinuteAverage);
+            //CalculateAveragesFor(datapoint, 60 * 20, DataPoint.Type.Data20MinuteAverage);
+        }
+
+        private void CalculateAveragesFor(DataPoint dataPointJustAdded, ushort spanSeconds, DataPoint.Type type)
+        {
+            List<ushort> dataForAverage = new List<ushort>();
+
+            DataPoint currentDataPoint;
+            int currentIndex = this.IndexOf(dataPointJustAdded);
+            double currentTimeOffset=0;
+
+
+            Interlocked.Increment(ref _threadCount);
+            
+            int iIndex = currentIndex;
+            do
+            {
+                ushort dataValueRaw = 0;
+                currentDataPoint = this[iIndex];
+                
+                
+                dataValueRaw = currentDataPoint.DataValueRaw;
+                currentTimeOffset = currentDataPoint.TimeOffset;
+                
+                
+                dataForAverage.Add(dataValueRaw);
+                iIndex--;
+            }
+            while (iIndex >= 0 && dataPointJustAdded.TimeOffset - currentTimeOffset < spanSeconds * 1000);
+
+            dataPointJustAdded.DataValues[(int)type] = Convert.ToUInt16( dataForAverage.Average(num => Convert.ToUInt16(num)));
+
+            Interlocked.Decrement(ref _threadCount);
+            
+        }
+
+        public ushort GetBestAverageData(PeakswareTestOop.DataPoint.Type type)
+        {
+            DataPoint maxDatapoint = this[0];
+            foreach (DataPoint currentDataPoint in this)
+            {
+                
+                if(maxDatapoint.DataValues[(int)type] <  currentDataPoint.DataValues[(int)type] )
+                {
+                    maxDatapoint = currentDataPoint;
+                }
+            }
+
+            return maxDatapoint.DataValues[(int)type];
         }
     }
 
     class FitFile
     {
         public ChannelData _powerData = new ChannelData("Power");
+        public ChannelData _heartrateData = new ChannelData("HeartRate");
+        public ChannelData _cadenceData = new ChannelData("Cadence");
 
         public ChannelData PowerData { get { return _powerData; } }
+        public ChannelData HeartRate { get { return _heartrateData; } }
+        public ChannelData Cadence { get { return _cadenceData; } }
 
         public string FileName { get; private set; }
         private static System.DateTime? _start;
@@ -68,7 +237,7 @@ namespace PeakswareTestOop
 
                     // Connect the Broadcaster to our event source (in this case the Decoder)
                     decodeDemo.MesgEvent += mesgBroadcaster.OnMesg;
-
+                    
                     // Subscribe to message events of interest by connecting to the Broadcaster
                     mesgBroadcaster.RecordMesgEvent += new MesgEventHandler(OnRecordMesg);
                     mesgBroadcaster.FileIdMesgEvent += new MesgEventHandler(OnFileIDMesg);
@@ -79,6 +248,12 @@ namespace PeakswareTestOop
                     if (status)
                     {
                         decodeDemo.Read(fitSource);
+
+                        while (Cadence.ThreadCount > 0 || HeartRate.ThreadCount > 0 || PowerData.ThreadCount > 0) 
+                        {
+                            Thread.Sleep(10);
+                        }
+                        
                     }
                     else
                     {
@@ -99,12 +274,29 @@ namespace PeakswareTestOop
             var record = (RecordMesg)e.mesg;
 
             var power = record.GetFieldValue("Power");
+            var heartrate = record.GetFieldValue("HeartRate");
+            var cadence = record.GetFieldValue("Cadence");
 
-            if (power != null)
+            if (power != null || heartrate != null || cadence != null)
             {
                 double timeOffset = GetTimeOffset(record.GetTimestamp().GetDateTime());
-                DataPoint dataPoint = new DataPoint(timeOffset, (ushort)power);
-                _powerData.Add(timeOffset,dataPoint);
+                if (power != null)
+                {
+                    DataPoint dataPoint = new DataPoint(timeOffset, (ushort)power);
+                    _powerData.Add(timeOffset, dataPoint);
+                }
+
+                if (heartrate != null)
+                {
+                    DataPoint dataPoint = new DataPoint(timeOffset, Convert.ToUInt16( heartrate.ToString() ));
+                    _heartrateData.Add(timeOffset, dataPoint);
+                }
+
+                if (cadence != null)
+                {
+                    DataPoint dataPoint = new DataPoint(timeOffset, Convert.ToUInt16(cadence.ToString()));
+                    _cadenceData.Add(timeOffset, dataPoint);
+                }
             }
         }
 
--- PeaksWareCodingTest/2017-09-21-csharp-NET46/PeakswareTest/Program.cs
+++ PeaksWareCodingTest/2017-09-21-csharp-NET46/PeakswareTest/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Dynastream.Fit;
 using System.IO;
+using System.Threading;
 
 namespace PeakswareTest
 {
@@ -14,50 +15,96 @@ namespace PeakswareTest
 
         static void Main(string[] args)
         {
+            string strFilename = "..\\..\\files\\2012-05-31-11-17-12.fit";
+            System.DateTime start = System.DateTime.Now;
             Console.WriteLine("TrainingPeaks C# Code Test");
+            Console.WriteLine("input file {0}", strFilename);
 
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Usage: PeakswareTest.exe <filename>");
-                return;
-            }
+            //if (args.Length == 1)
+            //{
+            //    strFilename = args[0];
+                
+            //}
+            //Console.WriteLine("input file {0}", strFilename);
 
-            PeakswareTestOop.FitFile fitFile = new PeakswareTestOop.FitFile(args[0]);
+            PeakswareTestOop.FitFile fitFile = new PeakswareTestOop.FitFile(strFilename);
             fitFile.LoadDataAsync();
 
             // Attempt to open .FIT file
-            using (var fitSource = new FileStream(args[0], FileMode.Open))
-            {
-                Console.WriteLine("Opening {0}", args[0]);
+            //using (var fitSource = new FileStream(args[0], FileMode.Open))
+            //{
+            //    Console.WriteLine("Opening {0}", args[0]);
 
-                _powerChannel = new Dictionary<double, ushort>();
+            //    _powerChannel = new Dictionary<double, ushort>();
 
-                Decode decodeDemo = new Decode();
-                MesgBroadcaster mesgBroadcaster = new MesgBroadcaster();
+            //    Decode decodeDemo = new Decode();
+            //    MesgBroadcaster mesgBroadcaster = new MesgBroadcaster();
 
-                // Connect the Broadcaster to our event source (in this case the Decoder)
-                decodeDemo.MesgEvent += mesgBroadcaster.OnMesg;
+            //    // Connect the Broadcaster to our event source (in this case the Decoder)
+            //    decodeDemo.MesgEvent += mesgBroadcaster.OnMesg;
 
-                // Subscribe to message events of interest by connecting to the Broadcaster
-                mesgBroadcaster.RecordMesgEvent += new MesgEventHandler(OnRecordMesg);
-                mesgBroadcaster.FileIdMesgEvent += new MesgEventHandler(OnFileIDMesg);
+            //    // Subscribe to message events of interest by connecting to the Broadcaster
+            //    mesgBroadcaster.RecordMesgEvent += new MesgEventHandler(OnRecordMesg);
+            //    mesgBroadcaster.FileIdMesgEvent += new MesgEventHandler(OnFileIDMesg);
 
-                bool status = decodeDemo.IsFIT(fitSource) && decodeDemo.CheckIntegrity(fitSource);
+            //    bool status = decodeDemo.IsFIT(fitSource) && decodeDemo.CheckIntegrity(fitSource);
                 
-                // Process the file
-                if (status)
-                {
-                    Console.WriteLine("Decoding...");
-                    decodeDemo.Read(fitSource);
+            //    // Process the file
+            //    if (status)
+            //    {
+            //        Console.WriteLine("Decoding...");
+            //        decodeDemo.Read(fitSource);
 
-                    Console.WriteLine("Decoded FIT file {0}", args[0]);
-                }
-                else
-                {
-                    Console.WriteLine("Integrity Check Failed {0}", args[0]);
-                    decodeDemo.Read(fitSource);
-                }
+            //        Console.WriteLine("Decoded FIT file {0}", args[0]);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Integrity Check Failed {0}", args[0]);
+            //        decodeDemo.Read(fitSource);
+            //    }
+            //}
+
+            
+
+            foreach (PeakswareTestOop.DataPoint dataPoint in fitFile.PowerData)
+            {
+                Console.WriteLine("{0,7} {1,4} {2,4} {3,4} {4,4} {5,4} {6,4}", dataPoint.TimeOffset, dataPoint.DataValueRaw, dataPoint.DataValue1MinAvg, dataPoint.DataValue5MinAvg, dataPoint.DataValue10MinAvg, dataPoint.DataValue15MinAvg, dataPoint.DataValue20MinAvg);
             }
+
+            Console.WriteLine("Data1MinuteAverage {0} ", fitFile.PowerData.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data1MinuteAverage));
+            Console.WriteLine("Data5MinuteAverage {0} ", fitFile.PowerData.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data5MinuteAverage));
+            Console.WriteLine("Data10MinuteAverage {0} ", fitFile.PowerData.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data10MinuteAverage));
+            Console.WriteLine("Data15MinuteAverage {0} ", fitFile.PowerData.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data15MinuteAverage));
+            Console.WriteLine("Data20MinuteAverage {0} ", fitFile.PowerData.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data20MinuteAverage));
+
+
+            foreach (PeakswareTestOop.DataPoint dataPoint in fitFile.HeartRate)
+            {
+                Console.WriteLine("{0,7} {1,4} {2,4} {3,4} {4,4} {5,4} {6,4}", dataPoint.TimeOffset, dataPoint.DataValueRaw, dataPoint.DataValue1MinAvg, dataPoint.DataValue5MinAvg, dataPoint.DataValue10MinAvg, dataPoint.DataValue15MinAvg, dataPoint.DataValue20MinAvg);
+            }
+
+            Console.WriteLine("Data1MinuteAverage {0} ", fitFile.HeartRate.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data1MinuteAverage));
+            Console.WriteLine("Data5MinuteAverage {0} ", fitFile.HeartRate.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data5MinuteAverage));
+            Console.WriteLine("Data10MinuteAverage {0} ", fitFile.HeartRate.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data10MinuteAverage));
+            Console.WriteLine("Data15MinuteAverage {0} ", fitFile.HeartRate.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data15MinuteAverage));
+            Console.WriteLine("Data20MinuteAverage {0} ", fitFile.HeartRate.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data20MinuteAverage));
+
+            foreach (PeakswareTestOop.DataPoint dataPoint in fitFile.Cadence)
+            {
+                Console.WriteLine("{0,7} {1,4} {2,4} {3,4} {4,4} {5,4} {6,4}", dataPoint.TimeOffset, dataPoint.DataValueRaw, dataPoint.DataValue1MinAvg, dataPoint.DataValue5MinAvg, dataPoint.DataValue10MinAvg, dataPoint.DataValue15MinAvg, dataPoint.DataValue20MinAvg);
+            }
+
+            Console.WriteLine("Data1MinuteAverage {0} ", fitFile.Cadence.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data1MinuteAverage));
+            Console.WriteLine("Data5MinuteAverage {0} ", fitFile.Cadence.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data5MinuteAverage));
+            Console.WriteLine("Data10MinuteAverage {0} ", fitFile.Cadence.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data10MinuteAverage));
+            Console.WriteLine("Data15MinuteAverage {0} ", fitFile.Cadence.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data15MinuteAverage));
+            Console.WriteLine("Data20MinuteAverage {0} ", fitFile.Cadence.GetBestAverageData(PeakswareTestOop.DataPoint.Type.Data20MinuteAverage));
+
+
+
+            System.DateTime end = System.DateTime.Now;
+            Console.WriteLine("Done in {0} Seconds!", (end-start).TotalSeconds);
+            Console.ReadLine();
         }
 
         static void OnRecordMesg(object sender, MesgEventArgs e)
